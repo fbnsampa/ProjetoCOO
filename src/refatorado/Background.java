490b56a78a39c3253fb96489fb849196d3288bd0
Fabiano <fabiano.sampa@gmail.com>
2016-07-04 19:18:05 -0300

    Implementado estrutura dinâmica na classe background
    Os vetores de double que armazenavam os valores X e Y foram substituidos
    por ArrayList de Cordinates.

--- src/refatorado/Background.java
+++ src/refatorado/Background.java
@@ -1,61 +1,61 @@
 package refatorado;
 import java.awt.Color;
+import java.util.*;
 
 class Background {
 	
 	/* estrelas que formam o fundo de primeiro plano */
-	
-	double [] background1_X;
-	double [] background1_Y;
+	List <Cordinate> background1;
 	double background1_speed;
 	double background1_count;
 	
 	/* estrelas que formam o fundo de segundo plano */
-	double [] background2_X;
-	double [] background2_Y;
+	List <Cordinate> background2;
 	double background2_speed;
 	double background2_count;
 	
 	Background(){
-		background1_X = new double[20];
-		background1_Y = new double[20];
+		this.background1 = new ArrayList <Cordinate>();
+		this.background2 = new ArrayList <Cordinate>();
+		
 		background1_speed = 0.070;
 		background1_count = 0.0;
-		background2_X = new double[50];
-		background2_Y = new double[50];
 		background2_speed = 0.045;
 		background2_count = 0.0;
-		
-		for(int i = 0; i < background1_X.length; i++){
-			background1_X[i] = Math.random() * GameLib.WIDTH;
-			background1_Y[i] = Math.random() * GameLib.HEIGHT;
+
+		for (int i = 0; i < 20; i++){
+			Cordinate novo = new Cordinate(Math.random() * GameLib.WIDTH, Math.random() * GameLib.HEIGHT);
+			background1.add(novo);
 		}
 		
-		for(int i = 0; i < background2_X.length; i++){
-			background2_X[i] = Math.random() * GameLib.WIDTH;
-			background2_Y[i] = Math.random() * GameLib.HEIGHT;
+		for (int i = 0; i < 50; i++){
+			Cordinate novo = new Cordinate(Math.random() * GameLib.WIDTH, Math.random() * GameLib.HEIGHT);
+			background2.add(novo);
 		}
+		
 
 	}
 	
-	void desenhaBackground(){
+	void desenha(){
 		/* desenhando plano fundo distante */
 		
 		GameLib.setColor(Color.DARK_GRAY);
 		background2_count += background2_speed * Main.delta;
 		
-		for(int i = 0; i < background2_X.length; i++){
-			GameLib.fillRect(background2_X[i], (background2_Y[i] + background2_count) % GameLib.HEIGHT, 2, 2);
+		for(Cordinate aux : background2){
+			GameLib.fillRect(aux.x, (aux.y + background2_count) % GameLib.HEIGHT, 2, 2);
 		}
+		
 		
 		/* desenhando plano de fundo próximo */
 		
 		GameLib.setColor(Color.GRAY);
 		background1_count += background1_speed * Main.delta;
 		
-		for(int i = 0; i < background1_X.length; i++){
-			GameLib.fillRect(background1_X[i], (background1_Y[i] + background1_count) % GameLib.HEIGHT, 3, 3);
+		for(Cordinate aux : background1){
+			GameLib.fillRect(aux.x, (aux.y + background1_count) % GameLib.HEIGHT, 3, 3);
 		}
+		
 	}
 
 }
